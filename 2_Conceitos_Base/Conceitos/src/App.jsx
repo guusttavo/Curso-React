45ee7e949d0fd3bf6e544bce21511037d2b78b3d
guusttavo <gustavoribeiroda112@gmail.com>
2025-03-27 20:34:05 -0300

    continuação do curso de React

---
+++ 2_Conceitos_Base/Conceitos/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+import FirstComponent from './components/FirstComponent'
+
+function App() {
+  return <div className='App'>
+    <h1>Fundamentos do React</h1>
+    <FirstComponent/>
+  </div>;
+}
+
+export default App
